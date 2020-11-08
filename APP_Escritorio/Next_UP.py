7d5bcaed2849a5a8d7526c6517557d1aecff4eec
Michael Mendoza Gomez <52900339+Michelin8906@users.noreply.github.com>
2020-11-08 11:20:47 -0500

    Desarrollador3_vs1
    Creacion de Commit, cambio de comentario para el desarrolador 3

--- APP_Escritorio/Next_UP.py
+++ APP_Escritorio/Next_UP.py
@@ -10,7 +10,7 @@ import requests
 import json
 import datetime
 
-#Comentario desarrollo_2 Vs1
+#Comentario desarrollo_3 Vs1
 class Product:
     db_name = 'Next_U.db'
     _ENDPOINT = "https://api.binance.com"
